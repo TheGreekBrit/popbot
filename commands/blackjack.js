3cdec22528531c6f65c80cebd4a7c8515cc579ce
Alexander Houston <howdy140@gmail.com>
2019-05-21 19:22:44 -0700

    blackjack

---
+++ commands/blackjack.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	name: 'blackjack',
+	aliases: ['bj', 'cards'],
+	description: 'Starts a game of blackjack!.',
+	args: true,
+	usage: 'blackjack @player1 @player2 @player3',
+	helpDoc: 'placeholder',
+	execute(message, args) {
+		return message.reply('ERROR: no deck of cards found');
+	}
+};
